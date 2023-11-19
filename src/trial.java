ef9e78bb1693034b9c4e3031b7fb73f7fad5ef54
unknown <youstinamitcho@gmail.com>
2023-11-19 10:21:13 +0200

    trial

--- src/trial.java
+++ src/trial.java
@@ -7,19 +7,11 @@ public class trial {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    @Override
+    public String toString() {
+        return "trial{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
